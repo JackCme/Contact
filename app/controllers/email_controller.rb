a49299a60a684d6ebf128957c89a457eaa31f2e3
JackCme <pitou_106@naver.com>
2015-06-07 19:21:59 +0000

    이메일 보내기 기능 수정

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -8,14 +8,19 @@ class EmailController < ApplicationController
 		title = params[:title]
 		
 		gmail = Gmail.connect("cochonjun@gmail.com","Chldnjswns1!")
-		gmail.deliver do
-			to email
-			subject title
-			text_part do
-				body content
-			end
-		end
 
-		gmail.logout
+		if gmail.logged_in?
+			gmail.deliver do
+				to email
+				subject title
+				text_part do
+					body content
+				end
+			end
+			gmail.logout
+			@result = "Success!"
+		else
+			@result = "Failed to log in."
+		end
 	end
 end
